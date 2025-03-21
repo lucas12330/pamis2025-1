75beb34528a87dacf974b6baeeafaac18494bac6
lucas12330 <ligueketoniejudo@gmail.com>
2025-03-21 11:24:28 +0100

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # Programme de Lucas Bezanilla Bou
 # Adaptable selon le contexte du concour
 
+# Les direction sont sois CW sois CCW
+
 from microbit import *
 import maqueen
 
 start_time = -1  # Programme non démarré
 servoBool = True
 vide = True
+cotes = ""
+
+# Ajout des fonction de choix de cotès
+def Gauche():
+    cotes = "gauche"
+input.on_button_pressed(Button.A, Gauche)
+
+def Droite():
+    cotes = "droite"
+input.on_button_pressed(Button.B, Droite)
 
 def countTime():
-    global start_time, servoBool, vide
+    global start_time, servoBool, vide, cotes
 
     # Vérifier si le capteur P0 est activé et que le programme n'a pas encore démarré
     if start_time == -1 and pins.digital_read_pin(DigitalPin.P0) == 0:
@@ -18,31 +30,63 @@ def countTime():
     # Exécuter la logique seulement si le programme a commencé
     if start_time != -1:
         time = control.millis() - start_time  # Temps écoulé
+        
+        # Réaliser pour le cotès gauche
+        if (cotes == "gauche"):
 
-        if 3000<time < 9650:
-            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
-        elif time < 9850:
-            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
-            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-        elif time < 18000 and vide:
-            maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 17)
-        elif servoBool:
-            maqueen.motor_stop(maqueen.Motors.ALL)
-            for i in range(4):
-                maqueen.servo_run(maqueen.Servos.S1, 180)
-                basic.pause(200)
-                maqueen.servo_run(maqueen.Servos.S1, 0)
-                basic.pause(200)
-                maqueen.servo_run(maqueen.Servos.S1, 180)
-            servoBool = False
-        else:
-            maqueen.motor_stop(maqueen.Motors.ALL)
-
-basic.forever(countTime)
+            # Les temps sont a changé le jour du concour 85k<time<91650
+            if 3000<time < 9650:
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            # Les temps ici le jour du concour sont time<91850
+            elif time < 9850:
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+            #Pour cette action les temps sont time < 100000:
+            elif time < 18000 and vide:
+                maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 17)
+            elif servoBool:
+                maqueen.motor_stop(maqueen.Motors.ALL)
+                # Peut être modifier mais avec prudence suivant les recomandation des arbitre
+                # ainsi que des juges d'homologation
+                for i in range(4):
+                    maqueen.servo_run(maqueen.Servos.S1, 180)
+                    basic.pause(200)
+                    maqueen.servo_run(maqueen.Servos.S1, 0)
+                    basic.pause(200)
+                    maqueen.servo_run(maqueen.Servos.S1, 180)
+                servoBool = False
+            else:
+                maqueen.motor_stop(maqueen.Motors.ALL)
+        #Réaliser pour le cotès droit
+        elif (cotes == "droite"):
+            # Les temps sont a changé le jour du concour 85k<time<91650
+            if 3000<time < 9650: #Permet de faire passer cette action dans un intervalle de temps
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            # Les temps ici le jour du concour sont time<91850
+            elif time < 9850:
+                # Pour le cotès droit ils faut simplement changé le sens 
+                # De rotation du Pami
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+            #Pour cette action les temps sont time < 100000: 
+            elif time < 18000 and vide:
+                maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 17)
+            elif servoBool:
+                maqueen.motor_stop(maqueen.Motors.ALL)
+                for i in range(4):
+                    maqueen.servo_run(maqueen.Servos.S1, 180)
+                    basic.pause(200)
+                    maqueen.servo_run(maqueen.Servos.S1, 0)
+                    basic.pause(200)
+                    maqueen.servo_run(maqueen.Servos.S1, 180)
+                servoBool = False
+            else:
+                maqueen.motor_stop(maqueen.Motors.ALL)
+basic.forever(countTime) # ne jamais remplacer par des while true car ce sont des boucle fermé
 
 def on_forever():
     global vide
     vide = maqueen.ultrasonic() <= 5  # Met à jour `vide` automatiquement
-
-basic.forever(on_forever)
+basic.forever(on_forever) # Ne jamais remplacer par des while true car se sont des boucles fermé
--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 //  Programme de Lucas Bezanilla Bou
 //  Adaptable selon le contexte du concour
+//  Les direction sont sois CW sois CCW
 let start_time = -1
 //  Programme non démarré
 let servoBool = true
 let vide = true
+let cotes = ""
+//  Ajout des fonction de choix de cotès
+input.onButtonPressed(Button.A, function Gauche() {
+    let cotes = "gauche"
+})
+input.onButtonPressed(Button.B, function Droite() {
+    let cotes = "droite"
+})
 basic.forever(function countTime() {
     let time: number;
+    let i: number;
     
     //  Vérifier si le capteur P0 est activé et que le programme n'a pas encore démarré
     if (start_time == -1 && pins.digitalReadPin(DigitalPin.P0) == 0) {
@@ -17,31 +27,71 @@ basic.forever(function countTime() {
     if (start_time != -1) {
         time = control.millis() - start_time
         //  Temps écoulé
-        if (3000 < time && time < 9650) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
-        } else if (time < 9850) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-        } else if (time < 18000 && vide) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 17)
-        } else if (servoBool) {
-            maqueen.motorStop(maqueen.Motors.All)
-            for (let i = 0; i < 4; i++) {
-                maqueen.servoRun(maqueen.Servos.S1, 180)
-                basic.pause(200)
-                maqueen.servoRun(maqueen.Servos.S1, 0)
-                basic.pause(200)
-                maqueen.servoRun(maqueen.Servos.S1, 180)
+        //  Réaliser pour le cotès gauche
+        if (cotes == "gauche") {
+            //  Les temps sont a changé le jour du concour 85k<time<91650
+            if (3000 < time && time < 9650) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            } else if (time < 9850) {
+                //  Les temps ici le jour du concour sont time<91850
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+            } else if (time < 18000 && vide) {
+                // Pour cette action les temps sont time < 100000:
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 17)
+            } else if (servoBool) {
+                maqueen.motorStop(maqueen.Motors.All)
+                //  Peut être modifier mais avec prudence suivant les recomandation des arbitre
+                //  ainsi que des juges d'homologation
+                for (i = 0; i < 4; i++) {
+                    maqueen.servoRun(maqueen.Servos.S1, 180)
+                    basic.pause(200)
+                    maqueen.servoRun(maqueen.Servos.S1, 0)
+                    basic.pause(200)
+                    maqueen.servoRun(maqueen.Servos.S1, 180)
+                }
+                servoBool = false
+            } else {
+                maqueen.motorStop(maqueen.Motors.All)
             }
-            servoBool = false
-        } else {
-            maqueen.motorStop(maqueen.Motors.All)
+            
+        } else if (cotes == "droite") {
+            // Réaliser pour le cotès droit
+            //  Les temps sont a changé le jour du concour 85k<time<91650
+            if (3000 < time && time < 9650) {
+                // Permet de faire passer cette action dans un intervalle de temps
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            } else if (time < 9850) {
+                //  Les temps ici le jour du concour sont time<91850
+                //  Pour le cotès droit ils faut simplement changé le sens 
+                //  De rotation du Pami
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+            } else if (time < 18000 && vide) {
+                // Pour cette action les temps sont time < 100000: 
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 17)
+            } else if (servoBool) {
+                maqueen.motorStop(maqueen.Motors.All)
+                for (i = 0; i < 4; i++) {
+                    maqueen.servoRun(maqueen.Servos.S1, 180)
+                    basic.pause(200)
+                    maqueen.servoRun(maqueen.Servos.S1, 0)
+                    basic.pause(200)
+                    maqueen.servoRun(maqueen.Servos.S1, 180)
+                }
+                servoBool = false
+            } else {
+                maqueen.motorStop(maqueen.Motors.All)
+            }
+            
         }
         
     }
     
 })
+//  ne jamais remplacer par des while true car ce sont des boucle fermé
 //  Met à jour `vide` automatiquement
 basic.forever(function on_forever() {
     
